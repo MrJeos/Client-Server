4df3729ba60824960bc5c2fb7cab9f3c5e81fbfa
Ksenia <kseniazhd@yandex.ru>
2012-07-04 01:37:33 +0400

    Простые крестики-нолики(изменение)
    Signed-off-by: Ksenia <kseniazhd@yandex.ru>

--- tick-tack-toe/tick-tack-toe/Form1.Designer.cs
+++ tick-tack-toe/tick-tack-toe/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(62, 55);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(160, 140);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- tick-tack-toe/tick-tack-toe/Form1.cs
+++ tick-tack-toe/tick-tack-toe/Form1.cs
@@ -11,9 +11,93 @@ namespace tick_tack_toe
 {
     public partial class Form1 : Form
     {
+        private int[,] Pole = new int[3, 3];
+
+        private const int W = 40, H = 40;
+
+        private System.Drawing.Graphics g;
+
+        private int what = 0;
+
         public Form1()
         {
             InitializeComponent();
+
+            int row, col;
+
+            for (row = 0; row < 3; row++) 
+                for (col = 0; col < 3; col++)
+                    Pole[row,col] = 0;
+
+            g = panel1.CreateGraphics();
+
+        }
+
+        private void Show_game(Graphics g, int what)
+        {
+            for (int row = 0; row < 3; row++)
+                for (int col = 0; col < 3; col++)
+                    this.field(g, row, col, what);
+        }
+        private void field(Graphics g, int row, int col, int  what)
+        {
+            int x, y;
+
+            x = col * W + 1;
+            y = row * H + 1;
+
+            if (what == 0)
+            {
+                g.DrawRectangle(Pens.Black, x - 1, y - 1, W, H);
+            }
+
+            if (what == 1)
+            {
+                draw_cross(g, x, y);
+            }
+            
+            if (what == 2)
+            {
+                draw_nil(g, x, y);
+            }
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Show_game(g, what);
+            what = 1;
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int row = (int)(e.Y / H);
+            int col = (int)(e.X / W);
+
+            this.field(g, row, col, what);
+
+            if (what == 1)
+            {
+                Pole[row, col] = 1;
+                what = 2;
+            }
+            else
+            {
+                Pole[row,col] = 0;
+                what = 1;
+            }
+        }
+
+        private void draw_cross(Graphics g, int x, int y)
+        {
+            g.DrawLine(Pens.Black, x + 5, y + 5, x + W - 5, y + H - 5);
+            g.DrawLine(Pens.Black, x + W - 5, y + 5, x + 5, y + H - 5);
+        }
+
+        private void draw_nil(Graphics g, int x, int y)
+        {
+            g.DrawPie(Pens.Black, x, y, W, H, 0, 360);
+            g.FillPie(Brushes.White, x, y, W, H, 0, 360);
         }
     }
 }
