a6d65a1ce10df2f2152336ebf9624211b125e636
Artem Kochnev <artem-kochnev@mail.ru>
2012-07-03 16:32:30 +0400

    Начал работу над классом Server
    Signed-off-by: Artem Kochnev <artem-kochnev@mail.ru>

--- Server/Server/Server.cs
+++ Server/Server/Server.cs
@@ -5,27 +5,44 @@ using System.Text;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using System.Collections;
 
 namespace Server
 {
     class Server
     {
-        TcpListener Listener;
+        private TcpListener Listener;
+        private TcpClient Client;
+        private int port;
+        private IPAddress ipAdress = IPAddress.Any;
+        private bool work = false;
+        private Thread thrListener;
+        private Hashtable pairs1 = new Hashtable(30);
+        private Hashtable pairs2 = new Hashtable(30);
+        private TcpClient[] queue = new TcpClient[5];
 
-        public Server(int port)
+
+        public Server(int port = 4350, IPAddress ipAdress = null)
         {
-            Listener = new TcpListener(IPAddress.Any, port);
-            Listener.Start();
-
-            while (true)
+            this.port = port;
+            if (ipAdress != null)
             {
-                ThreadPool.QueueUserWorkItem(new WaitCallback(ClientThread), Listener.AcceptTcpClient());
+                this.ipAdress = ipAdress;
             }
         }
-        static void ClientThread(Object StateInfo)
+        public void StartListening()
         {
+            Listener = new TcpListener(ipAdress, port);
+            Listener.Start();
+            work = true;
+            thrListener = new Thread(KeepListening);
+        }
+        private void KeepListening()
+        {
+            while (work)
+            {
 
-            new Client((TcpClient)StateInfo);
+            }
         }
     }
 }
