30a3289aa3b427e58edcd3ba1ed42c5fb248f107
Artem Kochnev <artem-kochnev@mail.ru>
2012-07-03 12:51:52 +0400

    Небольшие обновления
    Signed-off-by: Artem Kochnev <artem-kochnev@mail.ru>

---
+++ Server/Server/Server.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Server
+{
+    class Server
+    {
+        TcpListener Listener;
+
+        public Server(int port)
+        {
+            Listener = new TcpListener(IPAddress.Any, port);
+            Listener.Start();
+
+            while (true)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(ClientThread), Listener.AcceptTcpClient());
+            }
+        }
+        static void ClientThread(Object StateInfo)
+        {
+
+            new Client((TcpClient)StateInfo);
+        }
+    }
+}
